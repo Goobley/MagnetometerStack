1c082d63d385e3529a0ab0bce875a7e6923eee89
cmo <c.osborne.1@research.gla.ac.uk>
2022-12-19 10:48:59 +0000

    Adding write batching to try and improve performance

--- ImportOldData.py
+++ ImportOldData.py
@@ -34,8 +34,12 @@ class MagSample:
 
         return f"{int(self.timestamp - midnight)} {self.data[0]} {self.data[1]} {self.data[2]} {self.data[3]}"
 
-    def submit_reading_influx(self, influx_bucket, influx_tag, write_api):
+    def to_point(self, influx_bucket, influx_tag):
         p = Point(influx_bucket).tag("instrument", influx_tag).field("east-west", self.data[0]).field("north-south", self.data[1]).field("up-down", self.data[2]).field("temperature", self.data[3]).time(self.timestamp, WritePrecision.MS)
+        return p
+
+    def submit_reading_influx(self, influx_bucket, influx_tag, write_api):
+        p = self.to_point(influx_bucket, influx_tag)
         write_api.write(bucket=influx_bucket, record=p)
 
     @classmethod
@@ -52,7 +56,10 @@ if __name__ == '__main__':
     for file in import_files:
         midnight = datetime.datetime.fromisoformat(f'{path.splitext(path.basename(file))[0]}T00:00:00+00:00').timestamp() * 1000
         data = np.genfromtxt(file)
+        file_samples = []
         for line_idx in range(data.shape[0]):
             sample = MagSample.from_array_line(data[line_idx, :], midnight)
-            sample.submit_reading_influx(InfluxBucket, InfluxTag, write_api)
+            file_samples.append(sample.to_point())
+        write_api.write(bucket=InfluxBucket, record=file_samples)
+
         print(f" => File '{file}' done.")
